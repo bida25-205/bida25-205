cd4a74b0bfbc64e6d865c2a0c3239ef14e23e4f1
bida25-205 <bida25-205@thuto.bac.ac.bw>
2026-04-22 09:48:00 +0200

    updates

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 console.log("JavaScript is connected!"); 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const feedbackForm = document.querySelector('.feedback-form');
+    const feedbackForm = document.getElementById('bakeryFeedbackForm');
     const successPopup = document.getElementById('successPopup');
 
     
     if (feedbackForm) {
-        feedbackForm.addEventListener('submit', function(e) {
+        feedbackForm.addEventListener('submit', async function(e) {
             e.preventDefault(); 
             
         
             const formData = new FormData(this);
 
-           
-            fetch(this.action, {
+        try {
+            const response = await fetch(this.action, {
                 method: 'POST',
                 body: formData,
                 headers: {
@@ -25,16 +25,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 successPopup.classList.remove('hidden');
                 feedbackForm.reset();
             } else {
-                alert("Oops! There was a problem. Please try again or message us on WhatsApp.");
+                alert("Submission failed. Please try again!");
             }
+         } catch (error) {
+            alert("There was an error connecting to server")
+         }
         });
+    
     }
 });
 
 
 function closePopup() {
-    const successPopup = document.getElementById('successPopup');
-    if (successPopup) {
-        successPopup.classList.add('hidden');
-    }
+    document.getElementById('successPopup').classList.add('hidden');
 }
