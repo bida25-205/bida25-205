da33b7bb2651d607f2cda8fb0c7deec02cddb71a
bida25-205 <bida25-205@thuto.bac.ac.bw>
2026-04-29 08:04:39 +0200

    updates

--- script.js
+++ script.js
@@ -57,3 +57,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+
+
+document.querySelectorAll(".faq-question").forEach(button => {
+  button.addEventListener("click", () => {
+    const answer = button.nextElementSibling;
+    answer.style.display =
+      answer.style.display === "block" ? "none" : "block";
+  });
+});
+
